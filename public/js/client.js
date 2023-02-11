8d1db52e63b9cc3993037838d32e5ee60333ff0c
jomoabhi <55912822+jomoabhi@users.noreply.github.com>
2023-02-11 14:43:21 +0530

    iniial7 commit

--- public/js/client.js
+++ public/js/client.js
@@ -1,64 +1,19 @@
 const backURL = `/nearestServer`;
 // const serverText = document.getElementById('servertext');
 const serverText = document.querySelector('#servertext');
-
+// import API_KEY from '../../../LoadBalancer/public/js/config';
+// console.log(API_KEY);
 let url = '';
-// console.log(serverText);
-// // const axios = require('axios');
-// src = 'https://unpkg.com/axios/dist/axios.min.js';
-// fetch(
-//   `https://ipgeolocation.abstractapi.com/v1/?api_key=${process.env.API_KEY}`,
-//   { method: 'GET' }
-// )
-//   .then((response) => {
-//     console.log(response.data);
-//   })
-//   .catch((error) => {
-//     console.log(error);
-//   });
-const getServer = (latitude, longitude) => {
-  body = {
-    latitude,
-    longitude,
-  };
-  fetch(backURL, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(body),
-  })
-    .then((res) => res.json())
-    .then((data, base_url) => {
-      // serverText.style.color = 'blue';
-      //   window.location = '/nearestServer/server';
-    });
-
-  // Requesting to underlying application server
-  //     const response = await axios({
-  //       url: `${backURL}`,
-  //       method: 'post',
-  //       headers: {
-  //         'Content-Type': 'application/json',
-  //       },
-  //       body: JSON.stringify(body),
-  //     });
-  //     // Send back the response data
-  //     // from application server to client
-  //     console.log(response);
-  //   } catch (err) {
-  //     // Send back the error message
-  //     // res.status(500).send('Client error!');
-  //     console.log('error', err);
-  //   }
-};
-
+// console.log(process.env.NEXT_PUBLIC_API_KEY);
+$.getJSON('http://www.geoplugin.net/json.gp', function (data) {
+  console.log(JSON.stringify(data, null, 2));
+});
 const successCallback = (position) => {
   console.log(position.coords);
   //   getServer();
-  body = {
+  const body = {
     latitude: position.coords.latitude,
-    longitude: position.coords.latitude,
+    longitude: position.coords.longitude,
   };
   //   };
   fetch(backURL, {
